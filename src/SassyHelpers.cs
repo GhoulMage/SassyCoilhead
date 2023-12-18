aacdd6df13d3f4be17e740d5d4463beced03773b
unknown <theghoulmage@gmail.com>
2023-12-18 01:49:57 +0100

    Removed unused code.

--- src/SassyHelpers.cs
+++ src/SassyHelpers.cs
@@ -10,18 +10,5 @@ namespace SassyCoilheadMod
             CoilheadDanceCheck danceScript = target.gameObject.AddComponent<CoilheadDanceCheck>();
             danceScript.SetSpringMan(target);
         }
-
-        internal static AnimatorOverrideController CreateAnimatorOverride(Animator animator, AnimationClip clip)
-        {
-            AnimatorOverrideController result = new AnimatorOverrideController(animator.runtimeAnimatorController);
-            var newAnimationClips = new List<KeyValuePair<AnimationClip, AnimationClip>>();
-            foreach (var animationClip in result.animationClips)
-            {
-                newAnimationClips.Add(new KeyValuePair<AnimationClip, AnimationClip>(animationClip, clip));
-            }
-            result.ApplyOverrides(newAnimationClips);
-
-            return result;
-        }
     }
 }
